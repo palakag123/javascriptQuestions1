c512e5cb1fdc300f7b0dd72511d5c86a68e242a7
Palak <palakagarwal1237@gmail.com>
2022-02-15 17:54:06 +0530

    feat: add test cases

--- findArmstrongNumber.js
+++ findArmstrongNumber.js
@@ -1,37 +1,44 @@
-function findNumberOfDigits(number)
-{
+function findNumberOfDigits(number){
+    if(number === undefined)
+        throw new Error('no number input');
+    if(number < 0)
+        throw new Error('negative numbers not valid input');
     let lenghtNumber=0;
-    while(number > 0)
-        {
-            r = Math.round(n%10);
+    while(number > 0){
             number = Math.floor(number/10);
             lenghtNumber += 1;
         }
         return lenghtNumber;
 }
 
-function findArmstrong(num)
-{
+function findArmstrong(num){
+    if(num === undefined)
+        throw new Error('no number input');
+    if(num < 0)
+        throw new Error('negative numbers not valid input');
+    let temp;
     let setOfArmstrong = [];
-    for (i = 0; i <= num; i++)
+    for (index = 0; index <= num; index++)
     {   
-        n=i;
-        let sum=0;
-        let numberOfDigits=findNumberOfDigits(i);
-        while(n > 0)
+        temp = index;
+        let sum = 0;
+        let numberOfDigits = findNumberOfDigits(index);
+        while(temp > 0)
         {
-            lastDigit = Math.round(n%10);
-            sum += Math.pow( lastDigit,numberOfDigits );
-            n = Math.floor(n/10);
+            lastDigit = Math.round(temp % 10);
+            sum += Math.pow( lastDigit, numberOfDigits );
+            temp = Math.floor(temp/10);
         }
-        if (sum === i)
-        {
+        if (sum === index){
             setOfArmstrong.push(sum)
         }
-        else
-            continue;
     }
-    return setOfArmstrong;
+    return setOfArmstrong.toString();
 }
 ans = findArmstrong(1700);
 console.log(ans);
+
+module.exports={
+    findArmstrong,
+    findNumberOfDigits
+}
